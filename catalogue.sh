8cab20d59dbb4ccb0e2ba6cafd9792f643086a49
LokeshV <lokeshviswa428@gmail.com>
2023-07-29 13:41:57 +0530

    Commit

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ yum install nodejs -y
 mkdir -p /app
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
