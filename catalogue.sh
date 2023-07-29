eb801da61e62203218fced4204eb89cf08a7451b
LokeshV <lokeshviswa428@gmail.com>
2023-07-29 13:48:26 +0530

    Commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,10 @@
 script_location=$(pwd)
-set -e
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 yum install nodejs -y
 
-#useradd roboshop
+useradd roboshop
 
 mkdir -p /app
 
@@ -20,3 +20,6 @@ systemctl daemon-reload
 
 systemctl enable catalogue
 systemctl restart catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
