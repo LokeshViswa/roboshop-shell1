a20beb0c1a42ac839bb18b738d8c5b166e5f1448
LokeshV <lokeshviswa428@gmail.com>
2023-07-29 14:02:25 +0530

    Commit

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,24 @@
 script_location=$(pwd)
 
+echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35m Remove Nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[35m Download frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
 cd /usr/share/nginx/html
+
+echo -e "\e[35m Extract Frontend content\e[0m"
 unzip /tmp/frontend.zip
 
+echo -e "\e[35m Copy Roboshop Nginx Configfile\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35m Restart Nginx\e[0m"
 systemctl restart nginx
