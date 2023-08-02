9770bb7da1d155543c59ff0713734d7b436cca98
LokeshV <lokeshviswa428@gmail.com>
2023-08-02 22:53:56 +0530

    Updating roboshop-shell1

--- catalogue.sh
+++ catalogue.sh
@@ -1,25 +1,46 @@
 script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
-echo -e "\e[35m Install Nginx\e[0m"
+echo -e "\e[35m Configure NodeJS repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+exit
+fi
 
-echo -e "\e[35m Install Nginx\e[0m"
+echo -e "\e[35m Install NodeJS\e[0m"
 yum install nodejs -y &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+exit
+fi
 
-echo -e "\e[35m Install Nginx\e[0m"
+echo -e "\e[35m Add Application User\e[0m"
 useradd roboshop &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+  echo "Refer Log file for more Information, LOG - ${LOG}"
+exit
+fi
 
-echo -e "\e[35m Install Nginx\e[0m"
 mkdir -p /app &>>${LOG}
 
-echo -e "\e[35m Install Nginx\e[0m"
+echo -e "\e[35m Downloading the App Content\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+exit
+fi
 
-echo -e "\e[35m Install Nginx\e[0m"
 rm -rf /app/* &>>${LOG}
-
-echo -e "\e[35m Install Nginx\e[0m"
 cd /app
 
 echo -e "\e[35m Install Nginx\e[0m"
@@ -27,7 +48,7 @@ unzip /tmp/catalogue.zip &>>${LOG}
 
 cd /app
 
-echo -e "\e[35m Install Nginx\e[0m"
+echo -e "\e[35m Install npm\e[0m"
 npm install &>>${LOG}
 
 echo -e "\e[35m Install Nginx\e[0m"
